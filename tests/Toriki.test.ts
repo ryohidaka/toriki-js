065f7ac1fc38bcbede237628d8ba28647dd02235
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-09-03 21:09:28 +0900

    test: categories() のテストコードを追加

--- tests/Toriki.test.ts
+++ tests/Toriki.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, beforeEach } from "vitest";
+import { describe, it, beforeEach, expect } from "vitest";
 
 import { Toriki } from "../src";
 
@@ -11,5 +11,37 @@ describe("Toriki", () => {
 
   it("初期化時にメニューをロードすること", () => {
     toriki.all();
+  });
+
+  it("全てのカテゴリを返すこと", () => {
+    const categories = toriki.categories();
+    expect(categories).toEqual([
+      "貴族焼",
+      "塩焼",
+      "たれ焼",
+      "串焼き",
+      "スピードメニュー",
+      "逸品料理",
+      "ご飯もの",
+      "デザート",
+      "ビール",
+      "メガ",
+      "チューハイ",
+      "レモンサワー",
+      "ハイボール",
+      "お茶ハイ",
+      "国産ジン",
+      "カクテル",
+      "果実酒",
+      "ワイン",
+      "焼酎",
+      "日本酒",
+      "梅酒",
+      "ノンアルコール",
+      "ソフトドリンク",
+      "クラフトジュース",
+      "コーヒー",
+      "数量限定",
+    ]);
   });
 });
