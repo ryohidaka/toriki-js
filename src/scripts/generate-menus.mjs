0b9fbfe6a297c2893b92a5ae77de7fb01dad5f85
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-09-03 20:15:58 +0900

    feat: メニュー一覧ファイル生成処理を追加

---
+++ src/scripts/generate-menus.mjs
@@ -0,0 +1,60 @@
+import fetch from "node-fetch";
+import { objectToCamel } from "ts-case-convert";
+import { Project } from "ts-morph";
+
+// データを取得してTypeScriptファイルに出力する関数
+async function fetchAndSaveJsonToTs() {
+  const url =
+    "https://raw.githubusercontent.com/ryohidaka/torikizoku-menus/main/dist/torikizoku.json";
+
+  try {
+    // JSONデータを取得
+    const response = await fetch(url);
+    const data = await response.json();
+    const menus = objectToCamel(data.menus);
+
+    // ts-morphを使用してTypeScriptファイルを生成
+    const project = new Project();
+    const sourceFile = project.createSourceFile("src/menus.ts", "", {
+      overwrite: true,
+    });
+
+    // 自動生成の警告コメントを追加
+    sourceFile.addStatements(
+      `// This file is auto-generated. Do not edit directly.`,
+    );
+    sourceFile.addStatements(`// @see ${url}`);
+
+    // ファイルにインポート文と型宣言を追加
+    sourceFile.addImportDeclaration({
+      namedImports: ["Menu"],
+      moduleSpecifier: "@/types",
+    });
+
+    // MENUS定数を追加
+    sourceFile.addVariableStatement({
+      declarationKind: "const",
+      declarations: [
+        {
+          name: "MENUS",
+          type: "Menu[]",
+          initializer: JSON.stringify(menus, null, 2).replace(
+            /"(\w+)":/g,
+            "$1:",
+          ),
+        },
+      ],
+      isExported: true,
+    });
+
+    // ファイルを書き出す
+    await sourceFile.save();
+
+    console.log("データをmenus.tsに保存しました");
+  } catch (error) {
+    console.error("データの取得または保存に失敗しました:", error);
+  }
+}
+
+// 関数を実行
+fetchAndSaveJsonToTs();
---
+++ src/types.ts
@@ -0,0 +1,15 @@
+/**
+ * メニュー
+ *
+ * @see https://github.com/ryohidaka/torikizoku-menus/blob/main/docs/database.md#%E3%83%87%E3%83%BC%E3%82%BF%E6%A7%8B%E9%80%A0
+ */
+export type Menu = {
+  // メニュー ID
+  id: number;
+  // メニュー名
+  name: string;
+  // カテゴリ名
+  category: string;
+  // 画像URL
+  imageUrl?: string;
+};
